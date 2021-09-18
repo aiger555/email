244bae92511d327ca933bbfbdc60fdb5b5615a45
Aisuluu Temirbekova <75246512+t7843@users.noreply.github.com>
2021-09-18 16:48:57 +0600

    Update HW.py

--- HW.py
+++ HW.py
@@ -1,19 +1,16 @@
 import smtplib
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from decouple import config
 
 
 from_email = config('from_email',default='')
 password = config('password',default='')
 
-
-msg = MIMEMultipart()
-
+Mes = MIMEMultipart()
 to_email = 'nuralieva.aik@gmail.com'
 message = 'Hi, it is a message with smtp!'
-
-msg.attach(MIMEText(message, 'Homework'))
+Mes.attach(MIMEText(message, 'Homework'))
 
 server = smtplib.SMTP('smtp.gmail.com: 587')
 server.starttls()
